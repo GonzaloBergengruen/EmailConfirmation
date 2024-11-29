cfbe05f158c338e98f5cecf1759d3698999d2e61
GonzaloBergengruen <bergengruengonzalo@gmail.com>
2024-11-29 15:15:00 -0300

    Email punca pero no

---
+++ EmailConfirmation/EmailConfirmation/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace EmailConfirmation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnEnviarCorreoClicked(object sender, EventArgs e)
+        {
+            await EnviarCorreoAsync();
+        }
+
+        public async Task EnviarCorreoAsync()
+        {
+            var email = new EmailMessage
+            {
+                Subject = "Confirmar ingrego",
+                Body = "Confirme el ingreso\n\n" +
+                       "Aceptar: [https://miaplicacion.com/confirmar?respuesta=true]\n" +
+                       "Rechazar: [https://miaplicacion.com/confirmar?respuesta=false]",
+                To = new List<string> { "bergengruengonzalo@gmail.com" }
+            };
+
+            try
+            {
+                await Email.ComposeAsync(email);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "No se pudo enviar el email", "Ok");
+            }
+        }
+    }
+
+}
